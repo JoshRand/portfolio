9c1f1ddb9458fdbf7a17477ab5f520ef34fadc77
Joshua Anderson <JoshR.AndersonEng@gmail.com>
2024-01-19 00:54:15 +0000

    update to nginx config

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,6 +4,7 @@ COMPOSE="/usr/bin/docker-compose --ansi never"
 DOCKER="/usr/bin/docker"
 
 cd /home/src/portfolio/
+##$COMPOSE run certbot renew --force-renewal && $COMPOSE kill -s SIGHUP webserver
 $COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
 ## for testing replace with ((--dry-run )) $COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
